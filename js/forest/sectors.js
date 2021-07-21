2a27bf0172e38d0c30cd3febeb87ae9e9a4cb2f4
Freshairkaboom <freshairkaboom@gmail.com>
2021-07-21 13:06:57 +0200

    Made sure you could go back to the forest after quest

--- js/forest/sectors.js
+++ js/forest/sectors.js
@@ -66,6 +66,7 @@ function sectorForest2(direction) {
 
             if (theMerchantQuest.completion == false) {
                 theMerchant();
+                time.event = true;
                 return;
             }
         }
