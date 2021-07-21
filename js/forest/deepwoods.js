02237ec184a5fb2af69ffc7ad4f369d9cd2b8bfc
Lars <45746485+Freshairkaboom@users.noreply.github.com>
2021-07-21 12:31:43 +0200

    Update deepwoods.js

--- js/forest/deepwoods.js
+++ js/forest/deepwoods.js
@@ -1,5 +1,5 @@
 function enterDeepWoods() {
-    if (pmvariables.level < 10 || 1>2) {
+    if (pmvariables.level < 10 || 1<2) {
         menu.status = "The bandits on the warning sign are known to be strong and fierce. Maybe you should heed the warning and come back when you are stronger."
         updateView();
     }
