b7f1d6c4c47a461713ba20c9229dd73ee0399550
Freshairkaboom <freshairkaboom@gmail.com>
2021-08-08 20:04:24 +0200

    Removed save and load buttons from start menu

--- js/startview.js
+++ js/startview.js
@@ -1,9 +1,6 @@
 
     function startView() {
         let html = `
-        <button class="save" onclick="saveGame()">Save</button>
-        <button class="load" onclick="loadGame()">Load</button>
-
         <div class="container">
 
         <h1 class="title">Endless Complexity</h1>
