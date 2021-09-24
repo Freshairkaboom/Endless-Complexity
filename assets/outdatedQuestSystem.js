832d746fb34b380bf732bc35cfd8c9faf05ed297
Freshairkaboom <freshairkaboom@gmail.com>
2021-09-24 09:14:30 +0200

    Revert "Revert "Switch to view + removed color from ActionButtons""
    This reverts commit d6773fd845cee81f609f05c9ccc8da391b48ddae.

--- assets/outdatedQuestSystem.js
+++ assets/outdatedQuestSystem.js
@@ -1,13 +1,13 @@
 function theMerchant() {
     menu.status = theMerchantQuest.intro[0];
 
-    button.firstaction = `<button class="actionButton exploreSequence" onclick="theMerchantChapter1()">Start quest</button>`;
+    button.firstaction = `<button class="actionButton" onclick="theMerchantChapter1()">Start quest</button>`;
 
-    button.secondaction = `<button class="actionButton exploreSequence" onclick="ignoreQuest()">Ignore quest</button>`;
+    button.secondaction = `<button class="actionButton" onclick="ignoreQuest()">Ignore quest</button>`;
 
-    button.thirdaction = `<button class="actionButton exploreSequence" onclick=""></button>`;
+    button.thirdaction = `<button class="actionButton" onclick=""></button>`;
 
-    button.fourthaction = `<button class="actionButton exploreSequence" onclick=""></button>`;
+    button.fourthaction = `<button class="actionButton" onclick=""></button>`;
 
     view();
 }
@@ -15,13 +15,13 @@ function theMerchant() {
 function theMerchantChapter1() {
     menu.status = theMerchantQuest.chapter1[0];
 
-    button.firstaction = `<button class="actionButton exploreSequence" onclick="pushStory(0)">${theMerchantQuest.responses.button1[0]}</button>`;
+    button.firstaction = `<button class="actionButton" onclick="pushStory(0)">${theMerchantQuest.responses.button1[0]}</button>`;
 
-    button.secondaction = `<button class="actionButton exploreSequence" onclick="pushStory(1)">${theMerchantQuest.responses.button1[0]}</button>`;
+    button.secondaction = `<button class="actionButton" onclick="pushStory(1)">${theMerchantQuest.responses.button1[0]}</button>`;
 
-    button.thirdaction = `<button class="actionButton exploreSequence" onclick=pushStory(2)">${theMerchantQuest.responses.button1[0]}</button>`;
+    button.thirdaction = `<button class="actionButton" onclick=pushStory(2)">${theMerchantQuest.responses.button1[0]}</button>`;
 
-    button.fourthaction = `<button class="actionButton exploreSequence" onclick="pushStory(3)">${theMerchantQuest.responses.button1[0]}</button>`;
+    button.fourthaction = `<button class="actionButton" onclick="pushStory(3)">${theMerchantQuest.responses.button1[0]}</button>`;
 
     view();
 
@@ -31,13 +31,13 @@ function theMerchantChapter1() {
 function ignoreQuest() {
     menu.status = "The figure is not interesting enough to pique your curiosity, so you head back to the road.";
 
-    button.firstaction = `<button class="actionButton exploreSequence" onclick=""></button>`;
+    button.firstaction = `<button class="actionButton" onclick=""></button>`;
 
-    button.secondaction = `<button class="actionButton exploreSequence" onclick="sectorForest1(direction.west)">Go west</button>`;
+    button.secondaction = `<button class="actionButton" onclick="sectorForest1(direction.west)">Go west</button>`;
 
-    button.thirdaction = `<button class="actionButton exploreSequence" onclick="sectorForest3(direction.east)">Go east</button>`;
+    button.thirdaction = `<button class="actionButton" onclick="sectorForest3(direction.east)">Go east</button>`;
 
-    button.fourthaction = `<button class="actionButton exploreSequence" onclick="sectorForest5(direction.south)">Go south</button>`;
+    button.fourthaction = `<button class="actionButton" onclick="sectorForest5(direction.south)">Go south</button>`;
 
     view();
 }
@@ -52,13 +52,13 @@ function pushStory(choice) {
             if (chapterPush == true && theMerchantQuest.chapter1[i+1][0]) {
                 menu.status = theMerchantQuest.chapter1[i+1][choice];
 
-                button.firstaction = `<button class="actionButton exploreSequence" onclick="pushStory(0)">${theMerchantQuest.responses.button1[i+1]}</button>`;
+                button.firstaction = `<button class="actionButton" onclick="pushStory(0)">${theMerchantQuest.responses.button1[i+1]}</button>`;
 
-                button.secondaction = `<button class="actionButton exploreSequence" onclick="pushStory(1)">${theMerchantQuest.responses.button2[i+1]}</button>`;
+                button.secondaction = `<button class="actionButton" onclick="pushStory(1)">${theMerchantQuest.responses.button2[i+1]}</button>`;
 
-                button.thirdaction = `<button class="actionButton exploreSequence" onclick=pushStory(2)">${theMerchantQuest.responses.button3[i+1]}</button>`;
+                button.thirdaction = `<button class="actionButton" onclick=pushStory(2)">${theMerchantQuest.responses.button3[i+1]}</button>`;
 
-                button.fourthaction = `<button class="actionButton exploreSequence" onclick="pushStory(3)">${theMerchantQuest.responses.button4[i+1]}</button>`;
+                button.fourthaction = `<button class="actionButton" onclick="pushStory(3)">${theMerchantQuest.responses.button4[i+1]}</button>`;
 
                 view();
                 return;
@@ -67,13 +67,13 @@ function pushStory(choice) {
             else if (chapterPush == true) {
                 menu.status = theMerchantQuest.chapter1[i+1];
 
-                button.firstaction = `<button class="actionButton exploreSequence" onclick="pushStory(0)">I'm looking for a quest</button>`;
+                button.firstaction = `<button class="actionButton" onclick="pushStory(0)">I'm looking for a quest</button>`;
 
-                button.secondaction = `<button class="actionButton exploreSequence" onclick="pushStory(1)">Is everything okay?</button>`;
+                button.secondaction = `<button class="actionButton" onclick="pushStory(1)">Is everything okay?</button>`;
 
-                button.thirdaction = `<button class="actionButton exploreSequence" onclick=pushStory(2)">Awfully clean hands you got</button>`;
+                button.thirdaction = `<button class="actionButton" onclick=pushStory(2)">Awfully clean hands you got</button>`;
 
-                button.fourthaction = `<button class="actionButton exploreSequence" onclick="pushStory(3)">*Leave*</button>`;
+                button.fourthaction = `<button class="actionButton" onclick="pushStory(3)">*Leave*</button>`;
 
                 view();
                 return;
