81f84a741992df83fd532db763aa1d3a892e3ac1
Freshairkaboom <freshairkaboom@gmail.com>
2021-09-27 14:09:33 +0200

    Revert "wd"
    This reverts commit da41ac741b34fbd9252640bc32641ec084aaf3a5.

--- js/forest/deepwoods.js
+++
@@ -1,6 +0,0 @@
-function enterDeepWoods() {
-    if (pmvariables.level < 10 || 1<2) {
-        menu.status = "The bandits on the warning sign are known to be strong and fierce. Maybe you should heed the warning and come back when you are stronger."
-        view();
-    }
-}
